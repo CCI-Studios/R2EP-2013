02fafbda52f73241c6b606b27b9c628cc4d56440
Jonathan Bennett <jbennett@ccistudios.com>
2013-03-27 16:53:34 -0400

    Using realname module to name user pages

--- sites/all/themes/r2ep/template.php
+++ sites/all/themes/r2ep/template.php
@@ -8,24 +8,3 @@ function r2ep_form_alter(&$form, &$form_state, $form_id) {
 		$form['search_block_form']['#attributes']['placeholder'] = t('Search');
 	}
 }
-
-function r2ep_preprocess_page(&$variables) {
-	$arg = arg();
-	if (count($arg) > 1 && $arg[0] == 'user' && is_numeric($arg[1]) && empty($arg[2])) {
-		$profile = profile2_load_by_user($arg[1], 'main');
-
-		if ($profile) {
-			$first = $profile->field_first_name['und'][0]['value'];
-			$last = $profile->field_last_name['und'][0]['value'];
-
-			if ($first && $last) {
-				$name = "$first $last";
-			} else {
-				$name = "$first$last";
-			}
-
-			$variables['title'] = $name;
-		}
-	}
-}
-?>
