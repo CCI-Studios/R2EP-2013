0855db258129818d708fc1a50c57db6abc51e476
Jonathan Bennett <jbennett@ccistudios.com>
2013-03-27 12:59:16 -0400

    More checks to fix errors on user title

--- sites/all/themes/r2ep/template.php
+++ sites/all/themes/r2ep/template.php
@@ -11,18 +11,21 @@ function r2ep_form_alter(&$form, &$form_state, $form_id) {
 
 function r2ep_preprocess_page(&$variables) {
 	$arg = arg();
-	if ($arg[0] == 'user' && is_numeric($arg[1]) && empty($arg[2])) {
+	if (count($arg) > 1 && $arg[0] == 'user' && is_numeric($arg[1]) && empty($arg[2])) {
 		$profile = profile2_load_by_user($arg[1], 'main');
-		$first = $profile->field_first_name['und'][0]['value'];
-		$last = $profile->field_last_name['und'][0]['value'];
 
-		if ($first && $last) {
-			$name = "$first $last";
-		} else {
-			$name = "$first$last";
+		if ($profile) {
+			$first = $profile->field_first_name['und'][0]['value'];
+			$last = $profile->field_last_name['und'][0]['value'];
+
+			if ($first && $last) {
+				$name = "$first $last";
+			} else {
+				$name = "$first$last";
+			}
+
+			$variables['title'] = $name;
 		}
-
-		$variables['title'] = $name;
 	}
 }
 ?>
