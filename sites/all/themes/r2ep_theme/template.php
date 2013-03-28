ba324f43fbae0ff72390d8bd8a9b9b50377265b9
Jonathan Bennett <jbennett@ccistudios.com>
2013-03-27 21:44:28 -0400

    renamed theme

--- sites/all/themes/r2ep_theme/template.php
+++ sites/all/themes/r2ep_theme/template.php
@@ -1,6 +1,6 @@
 <?php
 
-function r2ep_form_alter(&$form, &$form_state, $form_id) {
+function r2ep_theme_form_alter(&$form, &$form_state, $form_id) {
 
 	// modify search form
 	if ($form_id == 'search_block_form') {
