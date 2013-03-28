30467551c35a67e230369553677502da579ad080
Jonathan Bennett <jbennett@ccistudios.com>
2013-03-28 01:01:09 -0400

    Fixed inline fields

--- sites/all/themes/r2ep_theme/template.php
+++ sites/all/themes/r2ep_theme/template.php
@@ -8,3 +8,18 @@ function r2ep_theme_form_alter(&$form, &$form_state, $form_id) {
 		$form['search_block_form']['#attributes']['placeholder'] = t('Search');
 	}
 }
+
+function r2ep_preprocess_field(&$variables, $hook){
+
+	// remove inline clearfix
+    $element = $variables['element'];
+    if ($element['#label_display'] == 'inline') {
+        $classes_arr = &$variables['classes_array'];
+        for ($i = sizeof($classes_arr)-1; $i >= 0; $i--) {
+            if( $classes_arr[$i]==='clearfix' ){
+                unset($classes_arr[$i]);
+                $i=-1;
+            }
+    }
+  }
+}
