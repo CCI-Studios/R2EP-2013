c65fb92c7fbba701c3c1d3caea741b169c1197d4
Jonathan Bennett <jbennett@ccistudios.com>
2013-04-19 12:44:26 -0400

    added query menu dropdown

---
+++ sites/all/themes/r2ep_theme/js/scripts.js
@@ -0,0 +1,15 @@
+(function ($) {
+	$(function() {
+		$('ul.menu ul').show();
+		$('ul.menu ul').hide();
+
+		$('ul.menu li').hover(function() {
+			clearTimeout($.data(this, 'timer'));
+			$('ul', this).stop(true, true).slideDown(100);
+		}, function() {
+			$.data(this, 'timer', setTimeout($.proxy(function() {
+				$('ul', this).stop(true, true).slideUp(100);
+			}, this), 300));
+		});
+	});
+}(jQuery));
